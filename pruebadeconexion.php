6964853869123899b6322d92084b316e4a2028fa
camyoh <carlos.camyoh@gmail.com>
2018-05-29 21:19:10 -0500

    Leyendo datos desde phpmyadmin

--- pruebadeconexion.php
+++ pruebadeconexion.php
@@ -12,7 +12,10 @@
 <body>
 	<form id="form1" name="form1" method="post" action="pruebadeconexion.php">
 		<input type="text" name="nombre" id="texto" />
-		<input name="Submit" type="submit" value="Submit" />
+		<input name="Submit1" type="submit" value="Enviar" />
+	</form>
+    <form id="form2" name="form2" method="post" action="pruebadeconexion.php">
+		<input name="Submit2" type="submit" value="Leer" />
 	</form>
 
 </body>
@@ -20,12 +23,21 @@
 </html>
 
 <?php
-    if (isset($_POST['Submit'])) {
+    if (isset($_POST['Submit1'])) {
         $conexion = mysqli_connect('localhost', 'root', '');
         mysqli_select_db($conexion, 'local');
         $nombre = $_POST['nombre'];
         $nombre = utf8_decode($nombre);
         mysqli_query($conexion,"INSERT INTO nombre (nombre) VALUES ('$nombre')");
         echo 'Datos insertados';
+    }
+    if (isset($_POST['Submit2'])) {
+        $conexion = mysqli_connect('localhost', 'root', '');
+        mysqli_select_db($conexion, 'local');
+        $consulta = "SELECT nombre FROM nombre";
+        $datos = $conexion->query($consulta);
+        while($fila=$datos->fetch_assoc()){
+            echo utf8_decode($fila['nombre'])."<br>";
+        }
     }
 ?>
